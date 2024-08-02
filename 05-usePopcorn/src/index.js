8a81d03564914f5e8db7caa39de436fea6e92017
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-02 12:40:04 +0500

    working on making stars for rating

--- 05-usePopcorn/src/index.js
+++ 05-usePopcorn/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-// import App from './App';
-import StartRating from './StartRating';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import App from './App
+import StartRating from "./StartRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5}/>
+    <StartRating maxRating={5} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
