5a8752d4176b27a9f73c1438ac432893a306a3fc
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-19 03:34:52 +0500

    make the header,loader and error components

--- 07-react-quiz/src/App.js
+++ 07-react-quiz/src/App.js
@@ -1,10 +1,64 @@
-import DateCounter from "./DateCounter";
+import Headers from "./components/Header";
 import "./App.css";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import { useEffect } from "react";
+import { useReducer } from "react";
+import StartScreen from "./components/StartScreen";
+import Questions from "./components/Questions";
 
+const initialState = {
+  questions: [],
+  // 'loading','error','ready','active','finished'
+  status: "loading",
+  index: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Unkonow action");
+  }
+}
 function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQauestions = questions.Length;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Headers />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQauestions={numQauestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions question={questions[index]} />}
+      </Main>
     </div>
   );
 }
