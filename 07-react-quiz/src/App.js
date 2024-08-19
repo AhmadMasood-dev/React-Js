2f9c11cd65711cfdeef63a0195a74c1c22effd42
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-19 23:04:57 +0500

    make the next the button and correct out of total question and points and the progress bars

--- 07-react-quiz/src/App.js
+++ 07-react-quiz/src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 import Main from "./components/Main";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
-import { useEffect } from "react";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import StartScreen from "./components/StartScreen";
 import Questions from "./components/Questions";
+import NextQuestions from "./components/NextQuestions";
+import Progress from "./components/Progress";
 
 const initialState = {
   questions: [],
   // 'loading','error','ready','active','finished'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -32,16 +35,39 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestions":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
     default:
       throw new Error("Unkonow action");
   }
 }
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
-  const numQauestions = questions.Length;
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -55,9 +81,25 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && (
-          <StartScreen numQauestions={numQauestions} dispatch={dispatch} />
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Questions question={questions[index]} />}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Questions
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextQuestions dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
---
+++ 07-react-quiz/src/components/NextQuestions.jsx
@@ -0,0 +1,13 @@
+function NextQuestions({ dispatch, answer }) {
+  if (answer === null) return null;
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "nextQuestions" })}
+    >
+      Next
+    </button>
+  );
+}
+
+export default NextQuestions;
