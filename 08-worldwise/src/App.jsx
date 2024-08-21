abad749ea62b199fb261342655b59ab58eaa71b1
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-21 20:55:21 +0500

    make pageNav and import to other files for navbar

--- 08-worldwise/src/App.jsx
+++ 08-worldwise/src/App.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
-import AppNav from "./components/AppNav";
 import Pricing from "./Pages/Pricing";
 import Product from "./Pages/Product";
-
+import Login from "./Pages/Login";
+import PageNotFound from './Pages/PageNotFound'
+import AppLayout from "./Pages/AppLayout";
+import CityList from "./components/CityList";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="app" element={<AppNav />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} >
+        <Route index element={<CityList/>}/>
+          <Route path="cities" element={<p>Lisit of cities</p>}/>
+          <Route path="countries" element={<p>Lisit of countries</p>}/>
+          <Route path="form" element={<p>Form</p>}/>
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
