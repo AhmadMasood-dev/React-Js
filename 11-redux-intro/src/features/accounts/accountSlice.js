86a5c0fb3f324da4c4a3b1474e539ada2f77a461
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-09-03 02:19:28 +0500

    convert money according to USD currency and add to balance and use thunk and composeDevTool

--- 11-redux-intro/src/features/accounts/accountSlice.js
+++ 11-redux-intro/src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
-
+const host = "api.frankfurter.app";
 export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case "account/withdraw":
       return {
         ...state,
@@ -28,14 +33,30 @@ export default function accountReducer(state = initialState, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
-
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
