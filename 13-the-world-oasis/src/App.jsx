39cb7290c74b281ae105b422d1c0595f1b9fd527
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-09-26 20:35:50 +0500

    understand styled component library for the project and do some pratice

--- 13-the-world-oasis/src/App.jsx
+++ 13-the-world-oasis/src/App.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+const H1 = styled.h1`
+  font-size: 5rem;
+  font-weight: 900;
+  text-align: center;
+  background-color: var(--color-indigo-700);
+`;
+
 function App() {
-  return <div>Hello world</div>
+  return (
+    <>
+      <GlobalStyles />
+      <H1>Hello world</H1>;<Button>check in</Button>
+      <Button>check out</Button>
+      <Input type="text" placeholder="number of guests" />
+    </>
+  );
 }
 
 export default App;
