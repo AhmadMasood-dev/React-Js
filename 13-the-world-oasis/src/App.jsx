752404594af5c69581c9d69599782fe55be8563a
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-09-27 00:51:23 +0500

    understands props using styledcomponents

--- 13-the-world-oasis/src/App.jsx
+++ 13-the-world-oasis/src/App.jsx
@@ -2,19 +2,16 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-const H1 = styled.h1`
-  font-size: 5rem;
-  font-weight: 900;
-  text-align: center;
-  background-color: var(--color-indigo-700);
-`;
+import Heading from "./ui/Heading";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <H1>Hello world</H1>;<Button>check in</Button>
+      <Heading as="h1">Hello world</Heading>
+      <Button>check in</Button>
       <Button>check out</Button>
+      <Heading as="h3">Hello world</Heading>
       <Input type="text" placeholder="number of guests" />
     </>
   );
