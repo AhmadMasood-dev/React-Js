ac89fdf1a00e96d8b2959f338d001c6f45392ed5
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-10-01 00:44:17 +0500

    make different files and set routes

--- 13-the-world-oasis/src/App.jsx
+++ 13-the-world-oasis/src/App.jsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Dashboard from "./pages/Dashboard";
+import PageNotFound from "./pages/PageNotFound";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 function App() {
   return (
-    <BrowerRouter>
-      <Routes></Routes>
-    </BrowerRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />}></Route>
+          <Route path="Dashboard" element={<Dashboard />}></Route>
+          <Route path="bookings" element={<Bookings />}></Route>
+          <Route path="cabins" element={<Cabins />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="settings" element={<Settings />}></Route>
+          <Route path="account" element={<Account />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
