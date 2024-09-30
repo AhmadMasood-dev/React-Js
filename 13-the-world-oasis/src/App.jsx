db50c07ce20f6b77ee5d027a11e47da207eec224
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-09-30 23:53:35 +0500

    install react-router-domand start building pages

--- 13-the-world-oasis/src/App.jsx
+++ 13-the-world-oasis/src/App.jsx
@@ -1,19 +1,8 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <Heading as="h1">Hello world</Heading>
-      <Button>check in</Button>
-      <Button>check out</Button>
-      <Heading as="h3">Hello world</Heading>
-      <Input type="text" placeholder="number of guests" />
-    </>
+    <BrowerRouter>
+      <Routes></Routes>
+    </BrowerRouter>
   );
 }
 
