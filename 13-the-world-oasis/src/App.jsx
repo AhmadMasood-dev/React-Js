fce024d627419d5897ba2c09b12c608e106f80c6
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-10-01 22:30:49 +0500

    makesidebar,header and applout file and sidebar and header is same for every page

--- 13-the-world-oasis/src/App.jsx
+++ 13-the-world-oasis/src/App.jsx
@@ -10,19 +10,22 @@ import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
+import AppLayout from "./ui/AppLayout";
 function App() {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />}></Route>
-          <Route path="Dashboard" element={<Dashboard />}></Route>
-          <Route path="bookings" element={<Bookings />}></Route>
-          <Route path="cabins" element={<Cabins />}></Route>
-          <Route path="users" element={<Users />}></Route>
-          <Route path="settings" element={<Settings />}></Route>
-          <Route path="account" element={<Account />}></Route>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />}></Route>
+            <Route path="Dashboard" element={<Dashboard />}></Route>
+            <Route path="bookings" element={<Bookings />}></Route>
+            <Route path="cabins" element={<Cabins />}></Route>
+            <Route path="users" element={<Users />}></Route>
+            <Route path="settings" element={<Settings />}></Route>
+            <Route path="account" element={<Account />}></Route>
+          </Route>
           <Route path="login" element={<Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
