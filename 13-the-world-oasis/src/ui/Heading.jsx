e254b7b43e1fb6c6c5b6d4f1a1a29a9640fba2ec
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-11-06 23:51:43 +0500

    Authentication  User Login With Supabase

--- 13-the-world-oasis/src/ui/Heading.jsx
+++ 13-the-world-oasis/src/ui/Heading.jsx
@@ -4,17 +4,27 @@ const Heading = styled.h1`
   ${(props) =>
     props.as === "h1" &&
     css`
-      font-size: 5rem;
-      font-weight: 900;
-      text-align: center;
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+    ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
     `}
   ${(props) =>
     props.as === "h3" &&
     css`
-      font-size: 3rem;
-      font-weight: 700;
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 4rem;
+      font-weight: 600;
       text-align: center;
-      background-color: var(--color-indigo-500);
     `}
 `;
 
