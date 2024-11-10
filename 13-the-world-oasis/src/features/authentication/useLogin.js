d34da6bac841ce3bd8279930da476565afab5c0f
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-11-10 14:08:00 +0500

    when click on the logout button it goes to login page and user is also deleted form the local storage

--- 13-the-world-oasis/src/features/authentication/useLogin.js
+++ 13-the-world-oasis/src/features/authentication/useLogin.js
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
-    const queryClient=useQueryClient()
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-        queryClient.setQueriesData(['user'],user)
-      navigate("/dashboard");
+      queryClient.setQueriesData(["user"], user);
+      navigate("/dashboard", { replace: true });
       toast.success("Successfully logged in");
     },
     onError: (err) => {
